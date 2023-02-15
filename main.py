1d66beae7be6e5d05b6b91ef709ffca3d72ad2de
Amin <theaminux@gmail.com>
2023-02-15 19:53:24 +0300

    Nytimes completed

--- main.py
+++ main.py
@@ -1,43 +1,39 @@
 import json
 from extract.economist import Economist
-from extract.yenisafak import YeniSafak
+from extract.ny import NYTimes
+from extract.yenisafak import YeniSafak, YENI_SAFAK_QUERY
+
+# Economist
+def extract_from_economist():
+    economist = Economist()
+    ECONOMIST_PAGE_LIMIT = 273
+
+    economist.save_csv(
+        url="https://www.economist.com/finance-and-economics",
+        section="div",
+        value="css-e6sfh4 e1mrg8dy0",
+        pages=ECONOMIST_PAGE_LIMIT,
+    )
 
 
-economist = Economist()
-ECONOMIST_PAGE_LIMIT = 273
-
-economist.save_csv(
-    url="https://www.economist.com/finance-and-economics",
-    section="div",
-    value="css-e6sfh4 e1mrg8dy0",
-    pages=ECONOMIST_PAGE_LIMIT,
-)
-
-
-# print(json.dumps(articles, indent=4))
-
-# number = 10
-# for n in range(1, number + 1):
-#     print(f"Number {n}")
+# nytimes
+def extract_from_nytimes():
+    ny = NYTimes()
+    NY_NEWS_CATEGORIES = ["Business", "Your Money", "Financial", "Economic Analysis"]
+    START_DATE = "20150101"
+    END_DATE = "20230127"
+    PAGE_NUMBER = 201
+    ny.save_csv(NY_NEWS_CATEGORIES, START_DATE, END_DATE, PAGE_NUMBER)
 
 
-# Yenisafa news
-url = "https://api.piri.net/graphql/yenisafaken/"
-body = """
-{
-feed(page:1, limit:4,filter: {contentType: null, categories: ["606cb5649e7f71a2960c11cf"] })
-{
- 		title
-		publishDate
-		url
-}
-}
-"""
-
-# yeni = YeniSafak()
-# articles = yeni.get_articles(url=url, body=body)
-# print(json.dumps(articles, indent=4))
-# import requests
+def extract_from_yenisafak():
+    # Yenisafa news
+    url = "https://api.piri.net/graphql/yenisafaken/"
+    yeni = YeniSafak()
+    articles = yeni.get_articles(url=url, body=YENI_SAFAK_QUERY)
+    # print(json.dumps(articles, indent=4))
+    # limit 5050
 
 
-# limit 5050
+if __name__ == "__main__":
+    extract_from_nytimes()
